ce51cff3e774c98bb9c65bd0660b0ef68c19eae7
stkammari <saitejakammari8@gmail.com>
2025-08-05 06:52:10 -0700

    Water temperature

---
+++ Temperature.py
@@ -0,0 +1,12 @@
+temperature = float(input( "Enter the current water temperature : ") )
+if temperature > 40 :
+    print("it's hot")
+elif temperature > 30 :
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20 :
+    print("It's nice")
+else :
+    print("It's cold") 
+
+print("Done.")
