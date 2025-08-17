c54469e05da3393406c1f24ebd59dc61eed3cd7c
stkammari <saitejakammari8@gmail.com>
2025-08-17 08:13:37 -0700

    Predicting a virus with BP

---
+++ Notepad.py
@@ -0,0 +1,15 @@
+import tkinter 
+import os
+
+# To get the space above for the message  
+from tkinter.messagebox import *
+
+# To get the dialogue box 
+from tkinter.filedialog import *
+
+"""
+messagebox is used to write the message in the white 
+box called notepad and filedialog is used for the dialog box to appear
+ when you are opening file from anywhere in your system or saving your file in a particular position or place. 
+"""
+
